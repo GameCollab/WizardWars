f4cd259d3473d9be4a80abee2b1815235c5fedde
exdunn <ahdunn@uci.edu>
2016-12-18 18:19:24 -0800

    main menu testing

---
+++ WizardWars/Assets/Scenes/Menus/menuScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class menuScript : MonoBehaviour {
+
+    public Canvas quitMenu;
+    public Button startText;
+    public Button exitText;
+
+	// Use this for initialization
+	void Start () {
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        startText = startText.GetComponent<Button>();
+        startText = exitText.GetComponent<Button>();
+        quitMenu.enabled = false;
+    }
+
+    public void exitPress()
+    {
+        quitMenu.enabled = true;
+        startText.enabled = false;
+        exitText.enabled = false;
+    }
+
+    public void noPress()
+    {
+        quitMenu.enabled = false;
+        startText.enabled = true;
+        exitText.enabled = true;
+    }
+	
+    public void startLevel ()
+    {
+
+    }
+
+    public void exit ()
+    {
+        Application.Quit();
+    } 
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
